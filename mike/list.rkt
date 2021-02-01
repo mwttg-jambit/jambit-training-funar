c8bf1c5f06944ff8a5fa837c50ea841db6ee85b1
Mike Sperber <sperber@deinprogramm.de>
2021-02-01 16:18:12 +0100

    Update.

--- mike/list.rkt
+++ mike/list.rkt
@@ -1,3 +1,6 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname list) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Eine Liste ist eins der folgenden:
+; - die leere Liste
+; - eine Cons-Liste, bestehend aus erstem Element und Rest-Liste
