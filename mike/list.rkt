e82f2412605732cffd29d2dbea1081a72028bec1
Mike Sperber <sperber@deinprogramm.de>
2021-02-01 16:23:44 +0100

    Update.

--- mike/list.rkt
+++ mike/list.rkt
@@ -1,6 +1,24 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname list) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
-; Eine Liste ist eins der folgenden:
+; Eine Liste (von Zahlen) ist eins der folgenden:
 ; - die leere Liste
 ; - eine Cons-Liste, bestehend aus erstem Element und Rest-Liste
+;                                                          ^^^^^ Selbstbezug
+(define list-of-numbers
+  (signature (mixed empty-list cons-list)))
+
+(define-record empty-list
+  make-empty-list
+  empty?)
+
+(define empty (make-empty-list))
+
+; Eine Cons-Liste besteht aus:
+; - erstes Element
+; - Rest-Liste
+(define-record cons-list
+  cons
+  cons?
+  (first number)
+  (rest list-of-numbers))
