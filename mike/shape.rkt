5f749c240e0c7934a570d23e061b99347edcc596
Mike Sperber <sperber@deinprogramm.de>
2021-02-01 14:54:53 +0100

    Update.

---
+++ mike/shape.rkt
@@ -0,0 +1,9 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "beginner-reader.rkt" "deinprogramm" "sdp")((modname shape) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Eine geometrische Figur:
+; - Quadrat
+; - Kreis
+; - einer Überlagerung zweier geometrischer Figuren
+
+; Ist ein Punkt innerhalb einer geometrischen Figur?
