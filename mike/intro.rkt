c6059298e96dfabdd01dad3c85a63aa14961bbf9
Mike Sperber <sperber@deinprogramm.de>
2021-02-01 10:36:09 +0100

    Update.

--- mike/intro.rkt
+++ mike/intro.rkt
@@ -43,6 +43,48 @@
 
 ;(tile star1 circle1)
 
+; Datendefinition
+; Ein Haustier ist eins der folgenden: 
+; - Hund ODER
+; - Katze ODER
+; - Schlange
+; allgemein: Fallunterscheidung
+; hier: Aufzählung
+(define pet
+  (signature (enum "dog" "cat" "snake")))
+
+; Ist ein Haustier niedlich?
+(: cute? (pet -> boolean))
+
+(check-expect (cute? "dog") #t)
+(check-expect (cute? "cat") #t)
+(check-expect (cute? "snake") #f)
+
+; Gerüst
+#;(define cute?
+  (lambda (pet)
+    ...))
+
+; Schablone
+#;(define cute?
+  (lambda (pet)
+    (cond ; Verzweigung (wg. Fallunterscheidung)
+      ; Jeder Zweig (Bedingung Ergebnis)
+      ((string=? pet "dog") ...)
+      ((string=? pet "cat") ...)
+      ((string=? pet "snake") ...)
+      )))
+
+(define cute?
+  (lambda (pet)
+    (cond ; Verzweigung (wg. Fallunterscheidung)
+      ; Jeder Zweig (Bedingung Ergebnis)
+      ((string=? pet "dog") #t)
+      ((string=? pet "cat") #t)
+      ((string=? pet "snake") #f)
+      )))
+
+
 #|
 class C {
   int m(int x) {
