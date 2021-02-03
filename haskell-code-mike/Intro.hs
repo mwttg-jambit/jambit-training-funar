52e12a494cbc06cd7b3bcec9833f01a83655b339
Mike Sperber <sperber@deinprogramm.de>
2021-02-03 09:18:05 +0100

    Update.

--- haskell-code-mike/Intro.hs
+++ haskell-code-mike/Intro.hs
@@ -9,4 +9,13 @@ x = 42
 -- - Schlange
 -- data: neuer Datentyp
 data Pet = Dog | Cat | Snake
-deriving Show
+    deriving Show
+
+-- Großbuchstaben: "konstant"
+-- Kleinbuchstaben: Variable
+
+-- Ist Haustier niedlich?
+isCute :: Pet -> Bool
+isCute Dog = True
+isCute Cat = True
+isCute Snake = False
