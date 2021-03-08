99c42ae70399d4b88dfb486bf42ecca040f1bba8
Mike Sperber <sperber@deinprogramm.de>
2021-03-08 14:23:59 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Cards.scala
+++ scala-code-mike/src/main/scala/funar/Cards.scala
@@ -11,11 +11,11 @@ object Suit {
 }
 
 // total geordnet
-sealed trait Rank {
+sealed trait Rank { // Alternative abstract class Rank(value: Int)
   val value: Int
 } 
 object Rank {
-  case object Two extends Rank
+  case object Two extends Rank { value = 2 }
   case object Three extends Rank
   case object Four extends Rank
   case object Five extends Rank
