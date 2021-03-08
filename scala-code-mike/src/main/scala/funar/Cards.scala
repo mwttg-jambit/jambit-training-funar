f2a2fa708b17f52c6774761610c88c66d61e35dd
Mike Sperber <sperber@deinprogramm.de>
2021-03-08 14:24:54 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Cards.scala
+++ scala-code-mike/src/main/scala/funar/Cards.scala
@@ -16,18 +16,18 @@ sealed trait Rank { // Alternative abstract class Rank(value: Int)
 } 
 object Rank {
   case object Two extends Rank { val value = 2 }
-  case object Three extends Rank
-  case object Four extends Rank
-  case object Five extends Rank
-  case object Six extends Rank
-  case object Seven extends Rank
-  case object Eight extends Rank
-  case object Nine extends Rank
-  case object Ten extends Rank
-  case object Jack extends Rank
-  case object Queen extends Rank
-  case object King extends Rank
-  case object Ace extends Rank
+  case object Three extends Rank  { val value = 3 }
+  case object Four extends Rank { val value = 4 }
+  case object Five extends Rank { val value = 5 }
+  case object Six extends Rank { val value = 6 }
+  case object Seven extends Rank { val value = 7 }
+  case object Eight extends Rank { val value = 8 }
+  case object Nine extends Rank { val value = 9 }
+  case object Ten extends Rank { val value = 10 }
+  case object Jack extends Rank { val value = 11 }
+  case object Queen extends Rank { val value = 12 }
+  case object King extends Rank { val value = 13 }
+  case object Ace extends Rank { val value = 14 }
 
   val all: Seq[Rank] = Seq(Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, 
                            Jack, Queen, King, Ace)
