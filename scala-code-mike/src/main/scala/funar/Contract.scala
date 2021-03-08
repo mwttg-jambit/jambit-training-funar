8eb1b866eb6db7c256c23a89e10a489d06a04b49
Mike Sperber <sperber@deinprogramm.de>
2021-03-08 15:25:44 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Contract.scala
+++ scala-code-mike/src/main/scala/funar/Contract.scala
@@ -2,5 +2,17 @@ package funar
 
 /*
 Methode:
-1. 
+1. einfache Beispiele für Domänenobjekte einholen
+2. die modellieren
+*/
+
+/*
+Einfacher Vertrag:
+
+Bekomme 100Pfund am 29.1.2001
+
+Bekomme 200EUR am 31.12.2021
+
+Zero-Coupon Bond
+Zero-Bond
 */
