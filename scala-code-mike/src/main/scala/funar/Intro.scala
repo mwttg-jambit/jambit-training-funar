018abb0e1f137b4a55ebdb8db61e9d5022dad119
Mike Sperber <sperber@deinprogramm.de>
2021-03-08 09:53:24 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Intro.scala
+++ scala-code-mike/src/main/scala/funar/Intro.scala
@@ -2,4 +2,17 @@ package funar
 
 object Intro {
   val s: String = "Mike"
+
+  // data Pet = Dog | Cat | Snake
+  // gemischte Daten / Aufzählungen:
+  // sealed: keine Unterklassen außer denen in dieser Datei
+  // trait ... interface
+  sealed trait Pet
+  // Aufzählung:
+  case object Cat extends Pet
+  case object Dog extends Pet
+  case object Snake extends Pet
+
+  val p1: Pet = Cat
+  val p2 = Dog
 }
