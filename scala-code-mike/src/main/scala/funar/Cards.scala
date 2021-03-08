f467c8e69362b26510db658f5a21a6acc4c403f0
Mike Sperber <sperber@deinprogramm.de>
2021-03-08 14:08:03 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Cards.scala
+++ scala-code-mike/src/main/scala/funar/Cards.scala
@@ -33,5 +33,8 @@ object Rank {
 case class Card(suit: Suit, rank : Rank)
 
 object Card {
+  def cartesianProduct[A, B](as: Seq[A], bs: Seq[B]): Seq[(A, B)] = // Seq von 2-Tupeln aus jeweils A und B
+
+  def deck: Seq[Card] = ???
 
 }
