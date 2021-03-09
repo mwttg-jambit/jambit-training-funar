e1d7dd262108cfb006a82aa0bec9f6b5bf804a24
Mike Sperber <sperber@deinprogramm.de>
2021-03-09 16:34:11 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/DB.scala
+++ scala-code-mike/src/main/scala/funar/DB.scala
@@ -23,7 +23,7 @@ object DB {
 
   // "Datenbank-Programm mit Resultat vom Typ A"
   trait DB[A] {
-    def flatMap[B](next: A => DB[B]): DB[B] = splice(this)(next)
+    def flatMapx[B](next: A => DB[B]): DB[B] = splice(this)(next)
     def     map[B](   f: A =>    B): DB[B] =
       this match {
         case Get(key, callback) =>
