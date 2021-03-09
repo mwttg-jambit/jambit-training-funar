d360e593463d0c5358a8166988f665d002ac999b
Mike Sperber <sperber@deinprogramm.de>
2021-03-09 15:45:03 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/DB.scala
+++ scala-code-mike/src/main/scala/funar/DB.scala
@@ -8,7 +8,17 @@ y = get("Mike")
 return y
 */
 object DB {
+  /*
   trait DBCommand
   case class Put(key: String, value: Int) extends DBCommand
   case class Get(key: String) extends DBCommand
+
+  type DBProgram = List[DBCommand]
+
+  val p1 = List(Put("Mike", 15), Get("Mike"))
+  */
+
+  trait DB
+  case class Get(key: String, callback: Int => DB)
+  case class Put(key: String, value: Int, callback: Unit => DB)
 }
