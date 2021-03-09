6cbd040cd227ab9ed82ccbd9574e974c7e8ad7a8
Mike Sperber <sperber@deinprogramm.de>
2021-03-09 13:29:36 +0100

    Update.

--- hearts-mike/src/main/scala/funar/hearts/GameEvent.scala
+++ hearts-mike/src/main/scala/funar/hearts/GameEvent.scala
@@ -2,6 +2,21 @@ package funar.hearts
 
 /* Event-Sourcing
 
+vs. Data Warehouse: aktueller Zustand, wird verändert,
+Konsistenz durch Transaktionen
+in verteiltem Setting schwer performant & korrekt zu bekommen
+
+Event-Sourcing:
+Logbuch über alles, was passiert ist in der Organisation: Events
+
+- alles, was passiert IST: Vergangenheit
+- fachlich motiviert
+- darf redundant sein
+
+Gibt häufig auch noch Commands:
+- Wunsch, daß etwas in der Zukunft passieren soll
+
+BITTE NICHT DIESELBEN TYPEN / OBJEKTE!
 
 */
 sealed trait GameEvent
