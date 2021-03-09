2cbc6ea17ac269ea68481c3e123045ba735620c8
Mike Sperber <sperber@deinprogramm.de>
2021-03-09 15:47:40 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/DB.scala
+++ scala-code-mike/src/main/scala/funar/DB.scala
@@ -18,8 +18,15 @@ object DB {
   val p1 = List(Put("Mike", 15), Get("Mike"))
   */
 
-  trait DB
-  case class Get(key: String, callback: Int => DB) extends DB
-  case class Put(key: String, value: Int, callback: Unit => DB) extends DB
-  
+  trait DB[A]
+  case class Get(key: String, callback: Int => DB[A]) extends DB[A]
+  case class Put(key: String, value: Int, callback: Unit => DB[A]) extends DB[A]
+  case class Return(result: A)
+
+  val p1 =
+    Put("Mike", 15, (()) =>
+    Get("Mike", x =>
+    Put("Mike", x + 1, (()) =>
+    Get("Mike", y =>
+
 }
