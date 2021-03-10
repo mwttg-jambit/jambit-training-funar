3ce0e6a6c8a9f28c1f0c8c0caf8405b8ab87b4f1
Mike Sperber <sperber@deinprogramm.de>
2021-03-10 09:16:23 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Monads.scala
+++ scala-code-mike/src/main/scala/funar/Monads.scala
@@ -6,6 +6,12 @@ object Monads {
     def map[A, B](x: F[A])(f: A => B): F[B]
   }
 
+  // Jede Monade ist ein Funktor, aber es gibt auch
+  // Funktoren, die keine Monade sind.
+
+  // dazwischen: Applicative Functors
+  //             Selective Functors
+
   // geht nur für Typen mit Typparameter
   trait Monad[M[_]] { // higher-kinded type
     def pure[A](result: A): M[A]
