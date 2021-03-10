b019c009cc832e6a26c551d386979a5f9e0aae16
Mike Sperber <sperber@deinprogramm.de>
2021-03-10 09:10:28 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Monads.scala
+++ scala-code-mike/src/main/scala/funar/Monads.scala
@@ -2,8 +2,9 @@ package funar
 
 object Monads {
   // geht nur für Typen mit Typparameter
-  trait Monad[M[_]] {
+  trait Monad[M[_]] { // higher-kinded type
     def pure[A](result: A): M[A]
+    def flatMap[A, B](m: M[A])(f: A => M[B]): M[B]
   }
 
 }
