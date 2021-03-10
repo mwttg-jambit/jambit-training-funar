e388b1f8b5b233f43d7f9ae9bb8671a49dbf433b
Mike Sperber <sperber@deinprogramm.de>
2021-03-10 10:36:31 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Monads.scala
+++ scala-code-mike/src/main/scala/funar/Monads.scala
@@ -74,9 +74,9 @@ object Monads {
   } yield (d1 + d2 + d2)
 
   case class Reader[Env, A](process: Env => A) {
-    def map[A, B](f: A => B): Reader[Env, B] = readerFunctor[Env].map(this, f)
+    def map[A, B](f: A => B): Reader[Env, B] = readerFunctor[Env].map(this)(f)
     def flatMap[A, B](f: A => Reader[Env, B]): Reader[Env, B] =
-      readerMonad[Env].flatMap(this, f)
+      readerMonad[Env].flatMap(this)(f)
   }
 
   def get[Env]: Reader[Env, Env] = Reader(x => x)
