956faf22e2478f14937db928464a07036b39b7df
Mike Sperber <sperber@deinprogramm.de>
2021-03-10 14:06:03 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Free.scala
+++ scala-code-mike/src/main/scala/funar/Free.scala
@@ -1,2 +1,12 @@
 package funar
 
+import cats._
+import cats.implicits._ 
+import cats.data._ 
+
+object Free {
+  // Reader, aber diesmal als Datentyp, unabhängig von der Implementierung
+  // wie DB
+  sealed trait Reader1[Env, A]
+  case class Get1[Env, A](callback: Env => Reader1[Env, A]) extends Reader[Env, A] 
+}
