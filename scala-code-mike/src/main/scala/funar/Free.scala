644973a61c82178a31a1aab2e082c9d64dd27dd5
Mike Sperber <sperber@deinprogramm.de>
2021-03-10 14:06:40 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Free.scala
+++ scala-code-mike/src/main/scala/funar/Free.scala
@@ -8,5 +8,7 @@ object Free {
   // Reader, aber diesmal als Datentyp, unabhängig von der Implementierung
   // wie DB
   sealed trait Reader1[Env, A]
-  case class Get1[Env, A](callback: Env => Reader1[Env, A]) extends Reader[Env, A] 
+  case class Get1[Env, A](callback: Env => Reader1[Env, A]) extends Reader1[Env, A] 
+  case class Return1[Env, A](result: A) extends Reader1[Env, A]
+
 }
