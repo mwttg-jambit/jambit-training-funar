0f6e058d29e437ed1027216a45fe613f125a4f8f
Mike Sperber <sperber@deinprogramm.de>
2021-03-10 10:28:52 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Monads.scala
+++ scala-code-mike/src/main/scala/funar/Monads.scala
@@ -81,7 +81,7 @@ object Monads {
   def readerMonad[Env] = new Monad[Reader[Env, *]] {
     def pure[A](a: A): Reader[Env, A] = Reader(_ => a)
     def flatMap[A, B](reader: Reader[Env, A])(f: A => Reader[Env, B]): Reader[Env, B] =
-      Reader(env => reader.process(env))
+      Reader(env => f(reader.proces(env)))
   }
 
 }
