ea40a7bbae8d6217ecf89da390c8e19b497d9db2
Mike Sperber <sperber@deinprogramm.de>
2021-03-10 14:18:49 +0100

    Update.

--- scala-code-mike/src/main/scala/funar/Free.scala
+++ scala-code-mike/src/main/scala/funar/Free.scala
@@ -8,10 +8,10 @@ object Free {
   // Reader, aber diesmal als Datentyp, unabhängig von der Implementierung
   // wie DB
   sealed trait Reader1[Env, A]
-  case class Get1[Env, A](callback: Env => Reader1[Env, A]) extends Reader1[Env, A] 
+  case class Ask1[Env, A](callback: Env => Reader1[Env, A]) extends Reader1[Env, A] 
   case class Return1[Env, A](result: A) extends Reader1[Env, A]
 
-  def get1[Env] : Reader1[Env, Env] = Get1(Return1(_))
+  def ask1[Env] : Reader1[Env, Env] = Ask1(Return1(_))
 
   // F ist nachher zuständig für die konkreten Operationen
   // F ist entweder DB', Reader'[Env, *]
@@ -20,7 +20,7 @@ object Free {
   case class Impure[F[_], A](f: F[Free[F, A]]) extends Free[F, A]
 
   sealed trait ReaderF[Env, SelfReference]
-  case class Get[Env, SelfReference](callback: Env => SelfReference) extends ReaderF[Env, SelfReference]
+  case class Ask[Env, SelfReference](callback: Env => SelfReference) extends ReaderF[Env, SelfReference]
 
   type IntReaderF[SelfReference] = ReaderF[Int, SelfReference]
   type IntReader[A] = Free[IntReaderF, A] 
